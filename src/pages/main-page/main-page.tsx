cbfadd89e18178cf567c6880fac248ae2dac92b5
xauroxs <xauroxs@gmail.com>
2024-02-12 16:56:01 +0300

    feat: set max width of layout to 1440px

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -8,9 +8,14 @@ import { MainPageFooter } from '@components/main-page/footer/footer';
 
 import bgLight from '@assets/main-page/bg-light.jpg';
 
+import classes from './main-page.module.css';
+
+const { container } = classes;
+
 export const MainPage: React.FC = () => {
     return (
         <Layout
+            className={container}
             style={{ background: `no-repeat center/cover url(${bgLight})`, minHeight: '100vh' }}
         >
             <MainPageSider />
