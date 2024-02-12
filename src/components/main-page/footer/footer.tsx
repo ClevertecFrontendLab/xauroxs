c25764ba4946d937420115ed73c55533596e036f
xauroxs <xauroxs@gmail.com>
2024-02-12 21:05:08 +0300

    feat: implement footer

--- src/components/main-page/footer/footer.tsx
+++ src/components/main-page/footer/footer.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Grid, Card } from 'antd';
+import { AndroidFilled, AppleFilled } from '@ant-design/icons';
+
+import classes from './footer.module.css';
+import './footer.css';
+
+const { useBreakpoint } = Grid;
+
+const { footer, reviews, downloadsCard, cardTitles, downloads, download } = classes;
 
 export const MainPageFooter: React.FC = () => {
-    return <Layout.Footer></Layout.Footer>;
+    const screens = useBreakpoint();
+
+    const footerPaddings = screens.md ? '24px 24px 42px' : '24px 16px 42px';
+
+    return (
+        <Layout.Footer
+            style={{
+                margin: 0,
+                padding: footerPaddings,
+                backgroundColor: 'transparent',
+            }}
+        >
+            <div className={footer}>
+                <p className={reviews}>Смотреть отзывы</p>
+                <Card
+                    className={downloadsCard}
+                    headStyle={{ padding: '0px 12px' }}
+                    bodyStyle={{ padding: '12px 0' }}
+                    title={
+                        <div className={cardTitles}>
+                            <span>Скачать на телефон</span>
+                            <span>Доступно в PRO-тарифе</span>
+                        </div>
+                    }
+                >
+                    <div className={downloads}>
+                        <div className={download}>
+                            <AndroidFilled />
+                            <span>Android OS</span>
+                        </div>
+                        <div className={download}>
+                            <AppleFilled />
+                            <span>Apple iOS</span>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        </Layout.Footer>
+    );
 };
